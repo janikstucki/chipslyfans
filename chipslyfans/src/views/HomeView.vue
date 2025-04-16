32df12b03b7023df8b3d46709db340cb8a5758d7
Luis Allamand <Luis@g-12.ch>
2025-04-17 00:05:54 +0200

    Added a fallback image

--- chipslyfans/src/views/HomeView.vue
+++ chipslyfans/src/views/HomeView.vue
@@ -22,10 +22,18 @@
             </div>
             <p class="mt-2 text-md fornt-bold line-clamp-2">{{ post.title }}</p>
             <p class="mt-2 text-sm line-clamp-2">{{ post.content }}</p>
-            <div v-for="(image, idx) in post.images" :key="idx">
-              <img :src="image.path" class="post-image">
+            <div
+              v-for="(img, index) in post.images"
+              :key="index"
+              class="relative group"
+            >
+              <img 
+                :src="img.url" 
+                class="..." 
+                @error="e => e.target.src = fallbackimage "
+              />
             </div>
-            <div class="flex space-x-4 text-gray-500 border-t border-gray-200 pt-4">
+                      <div class="flex space-x-4 text-gray-500 border-t border-gray-200 pt-4">
               <button class="flex items-center space-x-1 hover:text-indigo-600 " @click.stop="likepost(post.id)">
                 <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"/>
@@ -118,6 +126,7 @@
 </template>
 
 <script setup>
+import fallbackimage from '../assets/images/fallback.jpg'
 import { ref, computed, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import { useFetch } from '../helpers/getPosts';
