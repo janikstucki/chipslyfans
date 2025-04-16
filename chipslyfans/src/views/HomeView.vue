28aee9dce8adae120f876c1ba50fa03bbbf7afad
Luis Allamand <Luis@g-12.ch>
2025-04-17 01:00:33 +0200

    Better styling for images

--- chipslyfans/src/views/HomeView.vue
+++ chipslyfans/src/views/HomeView.vue
@@ -22,18 +22,22 @@
             </div>
             <p class="mt-2 text-md fornt-bold line-clamp-2">{{ post.title }}</p>
             <p class="mt-2 text-sm line-clamp-2">{{ post.content }}</p>
-            <div
-              v-for="(img, index) in post.images"
-              :key="index"
-              class="relative group"
-            >
-              <img 
-                :src="img.url" 
-                class="..." 
-                @error="e => e.target.src = fallbackimage "
-              />
-            </div>
-                      <div class="flex space-x-4 text-gray-500 border-t border-gray-200 pt-4">
+            <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+              <div class="flex flex-wrap gap-2">
+                <div class="flex flex-wrap gap-2">
+                  <div
+                    v-for="(img, index) in post.images"
+                    :key="index"
+                    class="w-[calc(43.333%-0.5rem)] max-w-[43.333%] rounded-md overflow-hidden">
+                      <img
+                        :src="img.url"
+                        @error="e => e.target.src = fallbackimage"
+                        class="w-full h-auto object-cover rounded-md border border-gray-200"/>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            <div class="flex space-x-4 text-gray-500 border-t border-gray-200 pt-4">
               <button class="flex items-center space-x-1 hover:text-indigo-600 " @click.stop="likepost(post.id)">
                 <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"/>
