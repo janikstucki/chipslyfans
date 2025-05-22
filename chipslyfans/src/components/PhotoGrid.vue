dddb26e05d53c792242b1655ecd49dba6feaa70c
Luis Allamand <Luis@g-12.ch>
2025-05-22 21:48:49 +0200

    Enhance PhotoGrid component by adding event.stopPropagation to image click events for better touch handling and preventing unintended interactions

--- chipslyfans/src/components/PhotoGrid.vue
+++ chipslyfans/src/components/PhotoGrid.vue
@@ -3,33 +3,33 @@
     <!-- Grid Layout ab sm -->
     <div v-if="!isMobile">
       <div v-if="count === 1" class="grid grid-cols-1 h-max-40">
-        <img :src="images?.[0]?.url" class="w-full h-auto object-cover rounded cursor-pointer" @click="$emit('zoom', images[0].url)" />
+        <img :src="images?.[0]?.url" class="w-full h-auto object-cover rounded cursor-pointer" @click.stop="$emit('zoom', images[0].url)" />
       </div>
 
       <div v-else-if="count === 2" class="grid grid-cols-2 gap-1 h-max-40">
-        <img v-for="(image, index) in images" :key="index" :src="image.url" class="w-full h-64 object-cover rounded cursor-pointer" @click="$emit('zoom', image.url)" />
+        <img v-for="(image, index) in images" :key="index" :src="image.url" class="w-full h-64 object-cover rounded cursor-pointer" @click.stop="$emit('zoom', image.url)" />
       </div>
 
       <div v-else-if="count === 3" class="grid grid-cols-3 gap-1 h-max-40">
-        <img :src="images?.[0]?.url" class="col-span-2 row-span-2 w-full h-full object-cover rounded cursor-pointer" @click="$emit('zoom', images[0].url)" />
+        <img :src="images?.[0]?.url" class="col-span-2 row-span-2 w-full h-full object-cover rounded cursor-pointer" @click.stop="$emit('zoom', images[0].url)" />
         <div class="grid grid-rows-2 gap-1">
-          <img v-for="(image, index) in images.slice(1)" :key="index" :src="image.url" class="w-full h-full object-cover rounded cursor-pointer" @click="$emit('zoom', image.url)" />
+          <img v-for="(image, index) in images.slice(1)" :key="index" :src="image.url" class="w-full h-full object-cover rounded cursor-pointer" @click.stop="$emit('zoom', image.url)" />
         </div>
       </div>
 
       <div v-else-if="count === 4" class="grid grid-cols-2 gap-1 h-max-40">
-        <img v-for="(image, index) in images" :key="index" :src="image.url" class="w-full h-48 object-cover rounded cursor-pointer" @click="$emit('zoom', image.url)" />
+        <img v-for="(image, index) in images" :key="index" :src="image.url" class="w-full h-48 object-cover rounded cursor-pointer" @click.stop="$emit('zoom', image.url)" />
       </div>
 
       <div v-else-if="count === 5" class="grid grid-cols-3 gap-1 h-max-40">
-        <img :src="images?.[0]?.url" class="col-span-2 row-span-2 w-full h-full object-cover rounded cursor-pointer" @click="$emit('zoom', images[0].url)" />
+        <img :src="images?.[0]?.url" class="col-span-2 row-span-2 w-full h-full object-cover rounded cursor-pointer" @click.stop="$emit('zoom', images[0].url)" />
         <div class="grid grid-rows-2 gap-1">
-          <img v-for="(image, index) in images.slice(1)" :key="index" :src="image.url" class="w-full h-full object-cover rounded cursor-pointer" @click="$emit('zoom', image.url)" />
+          <img v-for="(image, index) in images.slice(1)" :key="index" :src="image.url" class="w-full h-full object-cover rounded cursor-pointer" @click.stop="$emit('zoom', image.url)" />
         </div>
       </div>
 
       <div v-else class="grid grid-cols-3 gap-1 h-max-40">
-        <div v-for="(image, index) in visibleImages" :key="index" class="relative h-40 cursor-pointer" @click="$emit('zoom', image.url)">
+        <div v-for="(image, index) in visibleImages" :key="index" class="relative h-40 cursor-pointer" @click.stop="$emit('zoom', image.url)">
           <img :src="image.url" class="w-full h-full object-cover rounded" />
           <div v-if="showMore(index)" class="absolute inset-0 bg-black/60 text-white flex items-center justify-center text-xl font-bold">
             +{{ images.length - maxVisible }}
@@ -47,14 +47,14 @@
       <div class="flex transition-transform duration-300 ease-in-out"
            :style="{ transform: `translateX(-${currentIndex * 100}%)` }">
         <div v-for="(image, index) in images" :key="index" class="min-w-full h-60 flex items-center justify-center">
-          <img :src="image.url" class="object-cover w-full h-full cursor-zoom-in rounded" @click="$emit('zoom', image.url)" />
+          <img :src="image.url" class="object-cover w-full h-full cursor-zoom-in rounded" @click.stop="$emit('zoom', image.url)" />
         </div>
       </div>
-      <button v-if="currentIndex > 0" @click="currentIndex--"
+      <button v-if="currentIndex > 0" @click.stop="currentIndex--"
               class="absolute left-2 top-1/2 transform -translate-y-1/2 bg-black/50 text-white rounded-full p-2">
         ‹
       </button>
-      <button v-if="currentIndex < images.length - 1" @click="currentIndex++"
+      <button v-if="currentIndex < images.length - 1" @click.stop="currentIndex++"
               class="absolute right-2 top-1/2 transform -translate-y-1/2 bg-black/50 text-white rounded-full p-2">
         ›
       </button>
