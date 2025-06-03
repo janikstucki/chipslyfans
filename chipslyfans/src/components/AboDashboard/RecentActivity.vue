d8ca95f37457fe14059ab5979522cf129784ef04
Luis Allamand <Luis@g-12.ch>
2025-06-03 23:31:34 +0200

    Add RecentActivity component to AboDashboard for displaying user activity

---
+++ chipslyfans/src/components/AboDashboard/RecentActivity.vue
@@ -0,0 +1,19 @@
+<template>
+    <ul class="space-y-2 text-sm">
+        <li class="flex justify-between items-center">
+            <span><span class="font-semibold text-blue-600">Gianna</span> – 3h ago</span>
+        </li>
+        <li class="flex justify-between items-center">
+            <span><span class="font-semibold text-blue-600">Isabella</span> – renewed</span>
+        </li>
+        <li class="flex justify-between items-center">
+            <span><span class="font-semibold text-blue-600">Mason</span> – 1 day ago</span>
+        </li>
+        <li class="flex justify-between items-center">
+            <span><span class="font-semibold text-blue-600">Emma</span> – 2 days ago</span>
+        </li>
+        <li class="flex justify-between items-center">
+            <span><span class="font-semibold text-blue-600">Olivia</span> – renewed</span>
+        </li>
+    </ul>
+</template>
