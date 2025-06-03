50df8566eeb9dd908a8e0be44bfe9a470e3b6ae2
Luis Allamand <Luis@g-12.ch>
2025-06-03 23:28:40 +0200

    Add ChartCard, DonutChart, LineChart, BarChart, and RecentActivity components to AboDashboard for enhanced data visualization

--- chipslyfans/src/views/AboDashboard.vue
+++ chipslyfans/src/views/AboDashboard.vue
@@ -6,11 +6,33 @@
             <WidgetCard icon="UserPlusIcon" title="New Subscribers" value="32" change="" />
             <WidgetCard icon="ChartBarIcon" title="Revenue" value="498" change="+ $21" />
         </div>
+        <div class="flex flex-wrap justify-center gap-6 mt-8 max-w-[1200px] mx-auto">
+            <ChartCard title="Earnings">
+            <LineChart />
+            </ChartCard>
+
+            <ChartCard title="Subscribers">
+            <BarChart />
+            </ChartCard>
+
+            <ChartCard title="Renewal Rate">
+            <DonutChart :value="85" />
+            </ChartCard>
+
+            <ChartCard title="Recent Activity">
+            <RecentActivity />
+            </ChartCard>
+        </div>
     </div>
 </template>
 
 
 <script setup>
 import WidgetCard from '../components/AboDashboard/WidgetCard.vue';
+import ChartCard from '../components/AboDashboard/ChartCard.vue'
+import LineChart from '../components/AboDashboard/LineChart.vue'
+import BarChart from '../components/AboDashboard/BarChart.vue'
+import DonutChart from '../components/AboDashboard/DonutChart.vue'
+import RecentActivity from '../components/AboDashboard/RecentActivity.vue'
 import { CurrencyDollarIcon, UserGroupIcon, UserPlusIcon, ChartBarIcon } from '@heroicons/vue/24/outline';
 </script>
