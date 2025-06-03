3eee24c363118e9e401b43c1776febc3111009e4
Luis Allamand <Luis@g-12.ch>
2025-06-03 23:48:39 +0200

    Refactor AboDashboard layout for improved structure and spacing

--- chipslyfans/src/views/AboDashboard.vue
+++ chipslyfans/src/views/AboDashboard.vue
@@ -1,38 +1,52 @@
 <template>
-    <div class="flex justify-center px-4">
-        <div class="flex flex-wrap justify-center gap-6 max-w-[1200px] w-full">
+    <div class="px-4 py-6 flex flex-col items-center space-y-10">
+        <!-- Widgets -->
+        <div class="flex flex-wrap justify-center gap-6 w-full max-w-[1200px]">
             <WidgetCard icon="CurrencyDollarIcon" title="Earnings" value="$2,483" change="+ $248" />
             <WidgetCard icon="UserGroupIcon" title="Subscribers" value="290" change="+15" />
             <WidgetCard icon="UserPlusIcon" title="New Subscribers" value="32" change="" />
             <WidgetCard icon="ChartBarIcon" title="Revenue" value="498" change="+ $21" />
         </div>
-        <div class="flex flex-wrap justify-center gap-6 mt-8 max-w-[1200px] mx-auto">
-            <ChartCard title="Earnings">
-            <LineChart />
-            </ChartCard>
 
-            <ChartCard title="Subscribers">
-            <BarChart />
-            </ChartCard>
+        <!-- Chart Section -->
+        <div class="flex flex-wrap justify-center gap-6 w-full max-w-[1200px]">
+            <!-- Left column with Earnings + Subscribers -->
+            <div class="flex flex-col gap-6 w-full md:w-[500px]">
+                <ChartCard title="Earnings">
+                <div class="h-52">
+                    <LineChart />
+                </div>
+                </ChartCard>
 
-            <ChartCard title="Renewal Rate">
-            <DonutChart :value="85" />
-            </ChartCard>
+                <ChartCard title="Subscribers">
+                <div class="h-52">
+                    <BarChart />
+                </div>
+                </ChartCard>
+            </div>
 
-            <ChartCard title="Recent Activity">
-            <RecentActivity />
-            </ChartCard>
+            <!-- Donut & Recent Activity -->
+            <div class="flex flex-col gap-6 w-full sm:w-[300px]">
+                <ChartCard title="Renewal Rate">
+                    <div class="h-52 flex justify-center items-center">
+                        <DonutChart :value="85" />
+                    </div>
+                </ChartCard>
+
+                <ChartCard title="Recent Activity">
+                    <RecentActivity />
+                </ChartCard>
+            </div>
         </div>
     </div>
 </template>
 
-
 <script setup>
-import WidgetCard from '../components/AboDashboard/WidgetCard.vue';
-import ChartCard from '../components/AboDashboard/ChartCard.vue'
-import LineChart from '../components/AboDashboard/LineChart.vue'
-import BarChart from '../components/AboDashboard/BarChart.vue'
-import DonutChart from '../components/AboDashboard/DonutChart.vue'
-import RecentActivity from '../components/AboDashboard/RecentActivity.vue'
-import { CurrencyDollarIcon, UserGroupIcon, UserPlusIcon, ChartBarIcon } from '@heroicons/vue/24/outline';
+    import WidgetCard from '../components/AboDashboard/WidgetCard.vue';
+    import ChartCard from '../components/AboDashboard/ChartCard.vue';
+    import LineChart from '../components/AboDashboard/LineChart.vue';
+    import BarChart from '../components/AboDashboard/BarChart.vue';
+    import DonutChart from '../components/AboDashboard/DonutChart.vue';
+    import RecentActivity from '../components/AboDashboard/RecentActivity.vue';
+    import { CurrencyDollarIcon, UserGroupIcon, UserPlusIcon, ChartBarIcon } from '@heroicons/vue/24/outline';
 </script>
